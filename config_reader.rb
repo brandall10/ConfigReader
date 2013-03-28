d33bf4be8056d923cd5a8636221d7524882b13c1
Beau Randall <brandall10@gmail.com>
2013-03-28 15:40:13 -0700

    First commit

---
+++ config_reader.rb
@@ -0,0 +1,67 @@
+# ConfigReader - reads properties from a config file, ignores
+#   blank lines and single line comments, processes booleans properly
+#
+# Ex:
+# > load 'config_reader.rb'
+# => true
+# > test = ConfigReader.new("config_test.txt")
+# => <ConfigReader:0x007fc4b98fd4e8>...
+# > test.properties
+# => ["dir", "num", "boolfalse", "booltrue"] 
+# > test["dir"]
+# => "/here/there/everywhere"
+# > test["boolfalse"]
+# => false 
+
+class ConfigReader
+  @properties 
+
+  def initialize(file)
+    @properties = Hash.new(0)
+    process(file) 
+  end
+
+  # parse config file
+  def process(file)
+    begin
+      open(file).each do |line| 
+        parse(line)
+      end
+    rescue 
+      p "Can't open file."
+    end
+  end
+
+  # add to config hash if valid
+  def parse(line)
+
+    # ignore comments and blank lines
+    unless /^(\s*\#|\s*\n)/.match(line) 
+
+      # only handling case for key/val pairs, other parsing 
+      # considerations not defined in spec (ie. inline comments)
+      key, val = line.strip.split(/\s*=\s*/)  
+
+      # handle boolean cases
+      case val.downcase
+      when "on", "true"
+        val = true
+      when "off", "false"
+        val = false
+      end
+
+      # golden, add to hash
+      @properties[key] = val
+    end
+  end
+
+  # overload for easy access at object level
+  def [](property)
+    @properties[property]
+  end
+
+  # get a dump of the parsed properties
+  def properties
+    @properties.keys
+  end
+end
---
+++ config_spec.rb
@@ -0,0 +1,39 @@
+require './config_reader.rb'
+
+describe ConfigReader do
+
+  describe "Test line parse" do
+    before(:each) { @blank_config = ConfigReader.new("blank.txt") }
+
+    it "ignores comments" do
+      @blank_config.parse("# This is a comment")
+      @blank_config.properties.count.should eq(0) 
+    end
+
+    it "parses string val" do
+      @blank_config.parse("stuff = value")
+      @blank_config["stuff"].should eq("value")
+    end
+
+    it "parses bool val" do
+      @blank_config.parse("blah = false")
+      @blank_config["blah"].should eq(false)
+    end
+  end
+
+  describe "Test file parse" do
+    before(:all) { @test_config = ConfigReader.new("config_test.txt") }
+
+    it "consumes properties" do
+      @test_config.properties.count.should eq(4)
+    end
+
+    it "produces string val" do
+      @test_config["dir"].should be_an(String)  
+    end
+
+    it "produces boolean val" do
+      @test_config["booltrue"].should be_an(TrueClass)
+    end
+  end
+end
